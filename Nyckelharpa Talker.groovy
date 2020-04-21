b05f79f5be7cbb3e853f0dd5b73edf1baa09a282
arnbme <arnb@arnb.org>
2020-04-21 16:33:44 -0400

    V1.0.2 fix issues with erasing messages

--- Nyckelharpa Talker.groovy
+++ Nyckelharpa Talker.groovy
@@ -1,6 +1,6 @@
 /**
  *  Update Url:   https://raw.githubusercontent.com/arnbme/nyckelharpa/master/Nyckelharpa%20Talker.groovy
- * 
+ *
  *  Nyckelharpa Talker
  *  Supplements Big Talker adding speech when Nyckelharpa enters the Exit or Entry delay time period
  *		For LanNouncer Device: Chime, TTS text, Chime
@@ -10,7 +10,7 @@
  *	When speakers need different volumes, install multiple copies of this code
  *
  *
- *  Copyright 2017 Arn Burkhoff
+ *  Copyright 2017-2020 Arn Burkhoff
  *
  * 	Changes to Apache License
  *	4. Redistribution. Add paragraph 4e.
@@ -26,8 +26,12 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *	Dec 03, 2019 v0.1.0 Compensate for Hubitat Sonos driver missing command sendTextAndResume 
- *	Dec 02, 2019 v0.0.9 Correct logic and coding error with Do not talk times. 
+ *	Apr 21, 2020 v1.0.2 Document in input settings: erasing message stops it from talking.
+ *	Apr 21, 2020 v1.0.2 Issue: Erased msg returns on next time app page loads. Fix: When app is COMPLETE, do not set a default
+ *	Apr 20, 2020 v1.0.1 Check chime command for Open and close messages or error occurs during execution with no DH chime command
+ *	Apr 07, 2020 v1.0.0 Change default entry message to Disarm system or Police will arrive shortly
+ *	Dec 03, 2019 v0.1.0 Compensate for Hubitat Sonos driver missing command sendTextAndResume
+ *	Dec 02, 2019 v0.0.9 Correct logic and coding error with Do not talk times.
  *                      Allow it to be totally ignored by new setting theQuiet so fields do not have to be erased/empty
  *	May 23, 2019 v0.0.8 Allow each message to have it's own optional set of target devices
  *	May 18, 2019 v0.0.7 Minor text change on page two heading
@@ -46,9 +50,9 @@
  * 	Dec 17, 2018 v1.0.4 Change speaker capability audioNotification to musicPlayer. Did not select Sonos speakers
  * 	Nov 04, 2018 v1.0.3 Add support for generic quiet time per user request on messages
  *						Delayed messages are super delayed by unknown cloud processing error, allow for no chime and instant speak
- * 	Oct 21, 2018 v1.0.2	Support Arming Canceled messages from SHM Delay 
- * 	Jul 05, 2018 v1.0.1	correct non standard icon 
- * 	Jul 04, 2018 v1.0.1	Check for non Lannouner TTS devices and when true eliminate chime command 
+ * 	Oct 21, 2018 v1.0.2	Support Arming Canceled messages from SHM Delay
+ * 	Jul 05, 2018 v1.0.1	correct non standard icon
+ * 	Jul 04, 2018 v1.0.1	Check for non Lannouner TTS devices and when true eliminate chime command
  *	Jun 26, 2018 V1.0.0 Create from standalone module Keypad ExitDelay Talker
  */
 definition(
@@ -65,7 +69,7 @@ definition(
 
 def version()
 	{
-	return "0.1.0";
+	return "1.0.2";
 	}
 
 preferences {
@@ -87,7 +91,7 @@ def pageZeroVerify()
 		{
 		pageZero()
 		}
-	}	
+	}
 
 def pageZero()
 	{
@@ -98,13 +102,13 @@ def pageZero()
 			paragraph "This SmartApp, Nyckelharpa Talker, must be used as a child app of Nyckelharpa."
 			}
 		}
-	}	
+	}
 
 def pageOne()
 	{
 	dynamicPage(name: "pageOne", title: "Talker Messages and Devices", install: false, uninstall: true, nextPage: "pageOneVerify")
 		{
-		section("The Nyckelharpa Message Settings")
+		section("The Nyckelharpa Message Settings<br>Erasing a message stops it from talking")
 			{
 			if (state.error_data)
 				{
@@ -113,22 +117,56 @@ def pageOne()
 				}
 			input "logDebugs", "bool", required:true, defaultValue:false,
 				title: "Log debugging messages? Normally off/false"
-			input "theContactOpenMsg", "string", required: false, title: "Contact Open message, issued when system is disarmed: %device replaced with device name", 
-				defaultValue: "%device is now open"
+			if (app.getInstallationState()=='COMPLETE')
+				{
+				input "theContactOpenMsg", "string", required: false, title: "Contact Open message, issued when system is disarmed: %device replaced with device name"
+				}
+			else
+				{
+				input "theContactOpenMsg", "string", required: false, title: "Contact Open message, issued when system is disarmed: %device replaced with device name",
+					defaultValue: "%device is now open"
+				}
 			input "theOpenMsgChimes", "bool", defaultValue: true, required: false,
 				title: "Sound TTS Chimes with Open message Default: On/True"
-			input "theContactClosedMsg", "string", required: false, title: "Contact Closed message, issued when system is disarmed: %device replaced with device name", 
-				defaultValue: "%device is now closed"
+			if (app.getInstallationState()=='COMPLETE')
+				{
+				input "theContactClosedMsg", "string", required: false, title: "Contact Closed message, issued when system is disarmed: %device replaced with device name"
+				}
+			else
+				{
+				input "theContactClosedMsg", "string", required: false, title: "Contact Closed message, issued when system is disarmed: %device replaced with device name",
+					defaultValue: "%device is now closed"
+				}
 			input "theClosedMsgChimes", "bool", defaultValue: false, required: false,
 				title: "Sound TTS Chimes with Close message Default: Off/False"
-			input "theExitMsgKypd", "string", required: false, title: "Exit message: %nn replaced with delay seconds", 
-				defaultValue: "Alarm system is arming in %nn seconds. Please exit the facility"
-			input "theEntryMsg", "string", required: false, title: "Entry message: %nn replaced with delay seconds", 
-				defaultValue: "Please enter your pin on the keypad"
-			input "theArmMsg", "string", required: false, title: "Armed message: %hsmStatus replaced with HSM Arm State", 
-					defaultValue: "Alarm System is now armed in %hsmStatus Mode"
-			input "theDisarmMsg", "string", required: false, title: "Disarm message", 
-					defaultValue: "System Disarmed"
+			if (app.getInstallationState()=='COMPLETE')
+				{
+				input "theExitMsgKypd", "string", required: false, title: "Exit message: %nn replaced with delay seconds"
+				}
+			else
+				{
+				input "theExitMsgKypd", "string", required: false, title: "Exit message: %nn replaced with delay seconds",
+					defaultValue: "Alarm system is arming in %nn seconds. Please exit the facility"
+				}
+			if (app.getInstallationState()=='COMPLETE')
+				{
+				input "theEntryMsg", "string", required: false, title: "Entry message: %nn replaced with delay seconds"
+				}
+			else
+				{
+				input "theEntryMsg", "string", required: false, title: "Entry message: %nn replaced with delay seconds",
+					defaultValue: "Disarm system, or police will arrive shortly"
+				}
+			if (app.getInstallationState()=='COMPLETE')
+				input "theArmMsg", "string", required: false, title: "Armed message: %hsmStatus replaced with HSM Arm State"
+			else
+				input "theArmMsg", "string", required: false, title: "Armed message: %hsmStatus replaced with HSM Arm State",
+						defaultValue: "Alarm System is now armed in %hsmStatus Mode"
+			if (app.getInstallationState()=='COMPLETE')
+				input "theDisarmMsg", "string", required: false, title: "Disarm message"
+			else
+				input "theDisarmMsg", "string", required: false, title: "Disarm message",
+						defaultValue: "System Disarmed"
 			input "theQuiet", defaultValue: false, "bool", required: false,  submitOnChange: true,
 				title: "Do not talk during quiet time?  Default: Off - Talks all the time"
 			if (theQuiet)
@@ -146,11 +184,11 @@ def pageOne()
 				title: "Speaker Volume Level from 1 to 100"
 			if (parent.sendPushMessage)
 				paragraph "Global Default Notification Devices including Hubitat Phone App and Pushover: ${parent.sendPushMessage}"
-			else	
+			else
 				paragraph "No Default Notification Devices"
 			}
 		}
-	}	
+	}
 
 def pageOneVerify() 				//edit page one info, go to pageTwo when valid
 	{
@@ -161,11 +199,29 @@ def pageOneVerify() 				//edit page one info, go to pageTwo when valid
 			{
 			if (!it.hasCommand("chime"))
 				error_data="All TTS devices do not have Chime command, set 'Sound TTS Chimes' Off"
-			}	
-		}		
+			}
+		}
+	else
+	if (theOpenMsgChimes && theTTS)
+		{
+		theTTS.each
+			{
+			if (!it.hasCommand("chime"))
+				error_data="All TTS devices do not have Chime command, set 'Sound TTS Chimes with Open' Off"
+			}
+		}
+	else
+	if (theClosedMsgChimes && theTTS)
+		{
+		theTTS.each
+			{
+			if (!it.hasCommand("chime"))
+				error_data="All TTS devices do not have Chime command, set 'Sound TTS Chimes with Close' Off"
+			}
+		}
 	if (error_data != "")
 		{}
-	else	
+	else
 	if (!theQuiet)
 		{}
 	else
@@ -174,29 +230,29 @@ def pageOneVerify() 				//edit page one info, go to pageTwo when valid
 	else
 	if (theStartTime>"")
 		error_data="Please set do not talk end time or clear do not talk start time"
-	else	
+	else
 	if (theEndTime>"")
 		error_data="Please set do not talk start time or clear do not talk end time"
 
 	if (error_data!="")
 		{
-		state.error_data='<b>'+error_data.trim()+'</b>'
+		state.error_data='<b>ERROR: '+error_data.trim()+'</b>'
 		pageOne()
 		}
 	else
 		{
 		pageTwo()
 		}
-	}	
+	}
 
-//	This page summarizes the data prior to save	
+//	This page summarizes the data prior to save
 def pageTwo(error_data)
 	{
 	dynamicPage(name: "pageTwo", title: "Verify settings then tap Done, or tap this device's &lt;, &#9665;, or 'back' icon to change settings", install: true, uninstall: true)
 		{
 		def chimes=false
 		def chimetxt='(Chime) '
-		try 
+		try
 			{chimes=theSoundChimes}
 		catch(Exception e)
 			{}
@@ -210,8 +266,8 @@ def pageTwo(error_data)
 					paragraph "The Open Contact Message:\n(Chime) ${theContactOpenMsg}"
 				else
 					paragraph "The Open Contact Message:\n${theContactOpenMsg}"
-				}	
-			else	
+				}
+			else
 				paragraph "The Open Contact Message is not defined"
 			if (theContactClosedMsg)
 				{
@@ -219,53 +275,53 @@ def pageTwo(error_data)
 					paragraph "The Closed Contact Message:\n(Chime) ${theContactClosedMsg}"
 				else
 					paragraph "The Closed Contact Message:\n${theContactClosedMsg}"
-				}	
-			else	
+				}
+			else
 				paragraph "The Closed Contact Message is not defined"
 			if (theExitMsgKypd)
 				paragraph "The Exit Delay Message:\n${chimetxt}${theExitMsgKypd}"
-			else	
+			else
 				paragraph "The Exit Delay Message is not defined"
 			if (theEntryMsg)
 				paragraph "The Entry Delay Message:\n${chimetxt}${theEntryMsg}"
-			else	
+			else
 				paragraph "The Entry Delay Message is not defined"
 			if (theArmMsg)
 				paragraph "The Armed Message:\n${theArmMsg}"
-			else	
+			else
 				paragraph "The Armed Message is not defined"
 			if (theDisarmMsg)
 				paragraph "The Disarm Message:\n${theDisarmMsg}"
-			else	
+			else
 				paragraph "The Disarm Message is not defined"
 			if (theQuiet && theStartTime>"" && theEndTime>"")
-				paragraph "Quiet time active from ${theStartTime.substring(11,16)} to ${theEndTime.substring(11,16)}"	
+				paragraph "Quiet time active from ${theStartTime.substring(11,16)} to ${theEndTime.substring(11,16)}"
 			else
 				paragraph "Quiet time is inactive"
 
 			if (!chimes)
-				paragraph "Chimes do not sound with messages"	
+				paragraph "Chimes do not sound with messages"
 			if (theTTS)
 				paragraph "The Text To Speech Devices are ${theTTS}"
-			else	
+			else
 				paragraph "No Text To Speech Devices are defined"
 			if (theSpeakers)
 				{
 				paragraph "The Text To Speech Devices are ${theSpeakers}"
 				paragraph "The Speaker Volume Level is ${theVolume}"
 				}
-			else	
+			else
 				paragraph "No Speaker Devices are defined"
 			if (theVolume)
 				paragraph "Default Speaker Volume Level is $theVolume"
 			if (parent.sendPushMessage)
 				paragraph "Global Default Notification Devices including Hubitat Phone App and Pushover: ${parent.sendPushMessage}"
-			else	
+			else
 				paragraph "No default Notification Devices"
 			paragraph "Module Nyckelharpa Talker ${version()}"
-			}	
+			}
 		}
-	}	
+	}
 
 def installed() {
 	log.info "Installed with settings: ${settings}"
@@ -286,7 +342,7 @@ def initialize() {
 def TalkerHandler(evt)
 	{
 	logdebug("TalkerHandler entered, event: ${evt.value} ${evt?.data}")
-	def delaydata=evt?.data			//get the delay time or whatever was passed for message 
+	def delaydata=evt?.data			//get the delay time or whatever was passed for message
 	def msgout
 
 //	0.0.9 Dec 2, 2019 failing in Hubitat, totally revise code
@@ -294,33 +350,33 @@ def TalkerHandler(evt)
 /*	date formats
 Date: Mon Dec 02 14:02:09 EST 2019
 Time: 2019-12-02T00:00:00.000-0500
-*/	
+*/
 	if (theQuiet && theStartTime>"" && theEndTime>"")
 		{
 		def String nowis = new Date()
 		def timenow = nowis.substring(11,16)
 		logdebug ("${nowis} ${timenow} ${theStartTime.substring(11,16)} ${theEndTime.substring(11,16)}")
-		if (theEndTime.substring(11,16)>theStartTime.substring(11,16))	
+		if (theEndTime.substring(11,16)>theStartTime.substring(11,16))
 			{
-//			end > start 
+//			end > start
 			if (timenow>=theStartTime.substring(11,16) && timenow<theEndTime.substring(11,16))
 				{
-//				log.debug ("it is quiet time with end > start")	
+//				log.debug ("it is quiet time with end > start")
 				return false
 				}
-			}	
+			}
 		else
 //		end<start meaning: talk time is endtime to starttime
 		if (timenow>=theEndTime.substring(11,16) && timenow<=theStartTime.substring(11,16))
 			{
-//			log.debug ("it is time to talk with end < start")	
+//			log.debug ("it is time to talk with end < start")
 			}
 		else
 			{
-//			log.debug ("it is quiet time with end < start")	
+//			log.debug ("it is quiet time with end < start")
 			return false
 			}
-		}	
+		}
 //	else
 //		logdebug ('quiet time not active')
 
@@ -336,7 +392,7 @@ Time: 2019-12-02T00:00:00.000-0500
 			if (theSoundChimes)
 				theTTS.chime()
 			runInMillis(1800, ttsDelay, [data: [tts: msgout]])
-			}	
+			}
 		if (theSpeakers)
 			{
 //			theSpeakers.playTextAndResume(msgout,theVolume)
@@ -344,7 +400,7 @@ Time: 2019-12-02T00:00:00.000-0500
 				{
 				playTextAnd(msgout,theVolume,it)
 				}
-			}	
+			}
 		}
 	else
 	if (evt.value=="exitDelay" && theExitMsgKypd>"")
@@ -360,7 +416,7 @@ Time: 2019-12-02T00:00:00.000-0500
 				theTTS.chime()
 				runInMillis(1800, ttsDelay, [data: [tts: msgout]])
 				}
-			else		
+			else
 				{theTTS.speak(msgout)}
 			}
 		if (theSpeakers)
@@ -385,7 +441,7 @@ Time: 2019-12-02T00:00:00.000-0500
 				theTTS.chime()
 				runInMillis(1800, ttsDelay, [data: [tts: msgout]])
 				}
-			else		
+			else
 				{theTTS.speak(msgout)}
 			}
 		if (theSpeakers)
@@ -410,7 +466,7 @@ Time: 2019-12-02T00:00:00.000-0500
 				theTTS.chime()
 				runInMillis(1800, ttsDelay, [data: [tts: msgout]])
 				}
-			else		
+			else
 				{theTTS.speak(msgout)}
 			}
 		if (theSpeakers)
@@ -426,7 +482,7 @@ Time: 2019-12-02T00:00:00.000-0500
 	if (evt.value=="disarm" && theDisarmMsg>"")
 		{
 		if (theTTS)
-			{theTTS.speak(theDisarmMsg)}					
+			{theTTS.speak(theDisarmMsg)}
 		if (theSpeakers)
 			{
 //			{theSpeakers.playTextAndResume(theDisarmMsg,theVolume)}
@@ -442,7 +498,7 @@ Time: 2019-12-02T00:00:00.000-0500
 		def hsmState = [armedAway: "Away", armedHome: "Home", armedNight: "Night"][delaydata] ?: delaydata
 		msgout=theArmMsg.replaceAll("%hsmStatus",hsmState)
 		if (theTTS)
-			{theTTS.speak(msgout)}					
+			{theTTS.speak(msgout)}
 		if (theSpeakers)
 			{
 //			{theSpeakers.playTextAndResume(msgout,theVolume)}
@@ -456,7 +512,7 @@ Time: 2019-12-02T00:00:00.000-0500
 	if (evt.value=="ArmCancel" && delaydata>"")
 		{
 		if (theTTS)
-			{theTTS.speak(delaydata)}					
+			{theTTS.speak(delaydata)}
 		if (theSpeakers)
 			{
 //			{theSpeakers.playTextAndResume(delaydata,theVolume)}
@@ -468,13 +524,13 @@ Time: 2019-12-02T00:00:00.000-0500
 		}
 	else
 		{
-		if (!['ArmCancel','armed','disarm','contactClosedMsg', 'contactOpenMsg', 'exitDelay', 'entryDelay'].contains(evt.value)) 
+		if (!['ArmCancel','armed','disarm','contactClosedMsg', 'contactOpenMsg', 'exitDelay', 'entryDelay'].contains(evt.value))
 			log.warn "Nyckelharpa Talker, Unknown request: ${evt.value}"
 		}
 	}
 
 //	added V0.1.0 Dec 03, 2019
-//	Sonos driver from Hubitat does not have playTextAndResume, this is a workaround 
+//	Sonos driver from Hubitat does not have playTextAndResume, this is a workaround
 def playTextAnd(msg,vlm,speaker)
 	{
 	if (speaker.hasCommand("playTextAndResume"))
@@ -492,4 +548,4 @@ def logdebug(txt)
 	{
    	if (logDebugs)
    		log.debug ("${txt}")
-    }		
+    }
